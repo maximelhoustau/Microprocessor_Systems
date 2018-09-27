06b480e335910dcde370c42ec58a1c59ea74d5e7
Maxime Lhoustau <maxime.lhoustau@telecom-paristech.fr>
2018-09-27 17:33:33 +0200

    Modif exo

--- compilation/test.c
+++ compilation/test.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdint.h>
-/*
+
 uint32_t a=5; // global variable
 __attribute__((naked)) void f() {
     for (uint8_t i=0; i<=a; i++)
 g(); }
-*/
 
+/*
 // Global variables
     uint32_t *a;
     uint32_t *b;
     uint32_t *c;
 __attribute__((naked)) void f() { *a += *c;
 *b += *c; }
+*/
