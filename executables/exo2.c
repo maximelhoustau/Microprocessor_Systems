37e37fea5609a93c715c9634fadd14012bca33e1
Maxime Lhoustau <maxime.lhoustau@telecom-paristech.fr>
2018-10-03 11:32:42 +0200

    ajout de commentaires, interpretation des resultats à terminer

--- executables/exo2.c
+++ executables/exo2.c
@@ -5,10 +5,12 @@
 int test(int a, int b);
 int addition (int d, int e);
 
+//Definition de variables dans chacune des 3 sections dont la variable est le nom
 const int rodata = 10;
 int data = 11;
 int bss;
 
+//Definition de fonctions imbriquées pour determiner le sens de la pile
 int addition(int d, int e) {
 	printf("addresse de d: %p, addresse de e: %p \n", &d, &e);
 	int f = d + e;
@@ -22,10 +24,13 @@ int test(int a, int b) {
 }
 
 int main() {
+	//malloc pour accéder au tas
 	char * pointeur = malloc(20*sizeof(char));
+
 	printf("valeur de a + b = %d \n", test(3,4));
 	printf("adresse de rodata: %p, adresse de data: %p, adresse de bss: %p \n", &rodata, &data, &bss);
 	printf("addresse du pointeur malloc: %p \n", pointeur);
+	
 	free(pointeur);
 	return 0;
 }
