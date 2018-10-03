e3ed1598acdd1491b3a0a3742623a0ce004df28b
Maxime Lhoustau <maxime.lhoustau@telecom-paristech.fr>
2018-10-03 09:32:19 +0200

    modification du Makefile pour compiler sous Linux, corretion sur l'exo2

--- executables/exo2.c
+++ executables/exo2.c
@@ -1,11 +1,11 @@
 //Exercice destiné à trouver les @ mémoires des différentes sections sur Linux
 #include <stdio.h>
 
-const int text = 10;
+const int rodata = 10;
 int data = 11;
 int bss;
 
 int main() {
-  printf("adresse de text: %ls, adresse de data: %ls, adresse de bss: %ls", &text, &data, &bss);
+  printf("adresse de rodata: %p, adresse de data: %p, adresse de bss: %p \n", &rodata, &data, &bss);
   return 0;
 }
