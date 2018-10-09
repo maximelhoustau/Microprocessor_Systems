4a7f231b8c876fe8e7d1a66f092c496037e75e5d
maximelhoustau <maxime.lhoustau@telecom-paristech.fr>
2018-10-09 17:45:31 +0200

    Allumage/eteignage de la LED verte

--- TD_bare_metal/led.c
+++ TD_bare_metal/led.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 #include "led.h"
-#define REG_AHB2_GPIOB (*(volatile uint32_t *)0x4800004C)
+#define REG_AHB2_GPIOB (*(volatile uint32_t *)0x4002104C)
 #define REG_GPIOB_MODER (*(volatile uint32_t *)0x48000400)
 #define REG_GPIOB_BSSR (*(volatile uint32_t *)0x48000418)
 
@@ -8,11 +8,11 @@
 //registre GPIOB @début: 0x48000400 @fin:  0x480007FF taille 1Kb
 //
 //Activation de l'horloge:offset: 0x4C pour GPIOB bit 1 à 0/1 pour désactiver/activer
-//registre AHB2 activation de l'horloge @début: 0x50060C00 @fin: 0x48000000
+//registre AHB2 activation de l'horloge @début pour GPIOB: 0x48000400
 //
 void led_init(){
-	REG_AHB2_GPIOB = 0x0	;	
-	REG_GPIOB_MODER |=  (1<<28) & ~(1<<29);
+	REG_AHB2_GPIOB |= 0x2	;	
+	REG_GPIOB_MODER = (REG_GPIOB_MODER | (1<<28)) & ~(1<<29);
 }
 
 void led_g_on(){
