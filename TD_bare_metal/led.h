054f740ca9ac7a648ed88405a41c658ff4130b11
maximelhoustau <maxime.lhoustau@telecom-paristech.fr>
2018-10-12 11:09:41 +0200

    Fin du remplacement par les macros

--- TD_bare_metal/led.h
+++ TD_bare_metal/led.h
@@ -1,9 +1,6 @@
 #ifndef _LED_H_
 #define _LED_H_
 
-#define REG_GPIOB_MODER (*(volatile uint32_t *) 0x48000400)
-#define REG_GPIOC_MODER (*(volatile uint32_t *) 0x48000800)
-
 void led_init(void);
 void led_g_on(void);
 void led_g_off(void);
