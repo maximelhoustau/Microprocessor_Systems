d79bd9c575412098d19fd71fc758b7c859d3ea7b
maximelhoustau <maxime.lhoustau@telecom-paristech.fr>
2018-10-15 17:12:43 +0200

    initialisation de bank0 en cours

--- TD_bare_metal/matrix.h
+++ TD_bare_metal/matrix.h
@@ -38,6 +38,8 @@ void matrix_init(void);
 void deactivate_rows(void);
 void activate_rows(int row);
 void send_byte(uint8_t val, int bank);
+void mat_set_row(int row, const rgb_color *val);
+void init_bank0();
 
 typedef struct {
   uint8_t r;
