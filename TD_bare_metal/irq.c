06b55e386b9bc0939d8e9cec0a5e33d79e3aa0ab
maximelhoustau <maxime.lhoustau@telecom-paristech.fr>
2018-10-23 11:35:31 +0200

    Création du fichier irq.c et de la table des vecteurs d'interruption

---
+++ TD_bare_metal/irq.c
@@ -0,0 +1,28 @@
+
+
+void *vector_table[] = {
+    // Stack and Reset Handler
+    &_stack,            /* Top of stack */
+    _start,             /* Reset handler */
+
+    // ARM internal exceptions
+    NMI_Handler,        /* NMI handler */
+    HardFault_Handler,  /* Hard Fault handler */
+    MemManage_Handler, 
+    BusFault_Handler,
+    UsageFault_Handler,
+    0,                  /* Reserved */
+    0,                  /* Reserved */
+    0,                  /* Reserved */
+    0,                  /* Reserved */
+    SVC_Handler,        /* SVC handler */
+    0,                  /* Reserved */
+    0,                  /* Reserved */
+    PendSV_Handler,     /* Pending SVC handler */
+    SysTick_Handler,    /* SysTick hanlder */
+
+    // STM32L475 External interrupts
+    WWDG_IRQHandler,         /* Watchdog IRQ */
+    PVD_PVM_IRQHandler,      /* ... */
+    TAMP_STAMP_IRQHandler,   /* ... */
+}
